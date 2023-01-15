74ca98469f784862c55863824791e850b3e560c9
Макс <47178080+weise1@users.noreply.github.com>
2023-01-15 20:25:53 +0200

    fisrt

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// const result = confirm('Are you here?');
+
+// console.log(result);
+
+const answers = [];
+
+const firstName = prompt('Whats your name?: '),
+      lastName = prompt('You last name: ');
+
+answers[0] = firstName;
+answers[1] = lastName;
+
+console.log(answers);
+
+
