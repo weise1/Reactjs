bd18cc4f0d213dcaf30f7ff46e779f6fef3b83a6
Макс <47178080+weise1@users.noreply.github.com>
2023-01-17 19:02:33 +0200

    task2

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-/* Задание на урок:
-
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
-
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
-
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
-
-Проверить, чтобы все работало без ошибок в консоли */
-
 'use strict';
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
@@ -35,15 +12,29 @@ const personalMovieDB = {
     privat: false,
 };
 
-while (count <= numberOfFilms) {
-    fimls = prompt('Один из последних просмотренных фильмов?');
-    star = prompt('На сколько оцените его?');
-    personalMovieDB.movies[fimls] = star;
-    count++;
+for (let i = 0; i < numberOfFilms; i++) {
+    const fimls = prompt('Один из последних просмотренных фильмов?'),
+          star = prompt('На сколько оцените его?');
+
+    if (fimls != null && star != null && fimls != '' && star != '' && fimls.length < 50) {
+        personalMovieDB.movies[fimls] = star;
+        console.log('done');
+    } else {
+        i--;
+        console.log('eror');
+    }
+    
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (10 >= personalMovieDB.count && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
 }
 
 console.log(personalMovieDB);
-
-
-
 
